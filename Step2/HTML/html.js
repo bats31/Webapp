528754dd2c0474491fc14f14dada0f70e38ca7a3
bats31 <tobias_schlauch@gmx.de>
2021-04-19 12:03:25 +0200

    now Try

--- Step2/HTML/html.js
+++ Step2/HTML/html.js
@@ -1,15 +1,16 @@
 //const express = require('express');
+const { RSA_NO_PADDING } = require("constants");
 const http = require("http");
 
-//const host = 'http://lbbawebtest1.azurewebsites.net';
+const host = 'http://lbbawebtest1.azurewebsites.net';
 const port = 80;
 
 function sayHello(req,res){
     console.log("We've got a request for " + req.url);
     
+    res.statusCode = 200;
     // HTTP response header - the content will be HTML MIME type
-    res.writeHead(200, {'Content-Type': 'text/html'});
-
+    res.writeHeader('Content-Type': 'text/html');
     // Write out the HTTP response body
     res.write('<html><body>' +
     '<h1>Hello Dynamic World Wide Web<h1>'+
@@ -22,7 +23,7 @@ function sayHello(req,res){
 // Create the HTTP server
 
 const server = http.createServer(requestListener);
-server.listen(port, () => {
+server.listen(port, host, () => {
     console.log('Server is running on http://'+host+':'+port);
 });
 
