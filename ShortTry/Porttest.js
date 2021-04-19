0940c082b2e2bd3d19b96c92be1fe82bd8bebe58
bats31 <tobias_schlauch@gmx.de>
2021-04-19 12:47:24 +0200

    testport

--- ShortTry/Porttest.js
+++ ShortTry/Porttest.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const host = "lbbawebtest1.azurewebsites.net"
 const app = express()
 const port = 80
 
@@ -6,6 +7,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+app.listen(port, host, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
