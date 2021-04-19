ee5c8652124108d0465ff97b19ea7d3d60a57e29
bats31 <tobias_schlauch@gmx.de>
2021-04-19 13:44:36 +0200

    host

--- Step1/stringreturn.js
+++ Step1/stringreturn.js
@@ -9,7 +9,7 @@ const http = require("http");
 const host = 'https://lbbawebtest1.azurewebsites.net';
 
 //Connection on port 8080
-const port = '8080';
+const port = 80;
 
 //callable at 'http://localehost:8080'
 
